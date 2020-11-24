088aa2f9b7a77463e77d6435362f72a4b142f2d1
Ramon Reyes <37812464+ramon-rr@users.noreply.github.com>
2020-11-24 16:52:58 -0700

    current progress for levels list addition when clicking levels button.

---
+++ app/src/main/java/com/example/operationx/GameLevel.java
@@ -0,0 +1,14 @@
+package com.example.operationx;
+
+public class GameLevel {
+
+    int level;
+    String levelName;
+    int difficulty;
+    int characterImage;
+
+    public GameLevel(String levelName, int level){
+        this.levelName = levelName;
+        this.level = level;
+    }
+}
--- app/src/main/java/com/example/operationx/MainActivity.java
+++ app/src/main/java/com/example/operationx/MainActivity.java
@@ -1,20 +1,42 @@
 package com.example.operationx;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+import java.util.ArrayList;
+import java.util.logging.Level;
+
 public class MainActivity extends AppCompatActivity {
-// added comment test from AS
+    ArrayList<GameLevel> levels = new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.activity_main);
         setContentView(R.layout.activity_main);
 
-        System.out.println("Try to do something xd");
+        GameLevel level1 = new GameLevel("Cat Level", 1);
+        GameLevel level2 = new GameLevel("Plane Level", 2);
+        GameLevel level3 = new GameLevel("Pug Level", 3);
+
+        levels.add(level1);
+        levels.add(level2);
+        levels.add(level3);
+
+
+    }
+
+    public void showLevels(View view){
+        LevelsListFragment llf = new LevelsListFragment();
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.levelsList, llf, "LevelsListFrag")
+                .addToBackStack(null)
+                .commit();
+
+
     }
 
     public void startLevel(View view){
