c4d5240b90f3a601310ba5138311dfd4f24d2fcb
Ramon Reyes <37812464+ramon-rr@users.noreply.github.com>
2020-12-06 14:33:33 -0700

    Sharing button is functional, sends email instead of text due to lack of cellular connectivity in emulator.

--- app/src/main/java/com/example/operationx/SettingsFragment.java
+++ app/src/main/java/com/example/operationx/SettingsFragment.java
@@ -68,7 +68,7 @@ public class SettingsFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_settings, container, false);
 
-        SeekBar sBar = (SeekBar) view.findViewById(R.id.volumeSeekBar);
+        SeekBar sBar = view.findViewById(R.id.volumeSeekBar);
         SharedPreferences sharedPref = getActivity().getPreferences(Context.MODE_PRIVATE);
         final SharedPreferences.Editor editor = sharedPref.edit();
 
